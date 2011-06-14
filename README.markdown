4cd1e4c2d38bd0f284ff63b3f83284063fd429ca
Noel Hwande <noel.hwande@gmail.com>
2011-06-13 19:08:21 -0500

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Ruby on Rails Tutorial: My first app
+
+This is a sample Rails application from 
+[*Learning Rails by Example*] (http://railstutorial.org/) by [Michael Hartl] (http://michaelhartl.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd7d811f722f7f6135ff2f885be9b8aa8496bf33dade2010cd1f9ff108df81a1255b2d56536e46176abad5f871c074e43a1456d9473bc680b8ba30c3e879333a0'
