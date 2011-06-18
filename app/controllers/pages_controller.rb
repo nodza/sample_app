3dc6871759ade1fa51e3cbda0a7bb22c0f6ecc6e
Noel Hwande <noel.hwande@gmail.com>
2011-06-18 16:38:16 -0500

    Added Help page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,22 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    @base_title = "Rails Tutorial Sample App"
   end
 
   def contact
     @title = "Contact"
+    @base_title = "Rails Tutorial Sample App"
   end
   
   def about
     @title = "About"
+    @base_title = "Rails Tutorial Sample App"    
+  end
+  
+  def help
+    @title = "Help"
+    @base_title = "Rails Tutorial Sample App"    
   end
 
 end
